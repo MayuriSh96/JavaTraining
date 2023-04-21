6996904f2c3de067ba1b17e308e3fbd707acdfa1
Mayuri Shinde <mayurish@synerzipune.local>
2023-04-21 21:15:00 +0530

    Added code for calulator app

---
+++ Entry.java
@@ -0,0 +1,18 @@
+
+public class Entry {
+
+	public static void main(String[] args) {
+		
+		Maths m=new Maths();
+		int a=10;
+		int b=5;
+		m.add(a, b);
+		m.substract(a, b);
+		m.multiplication(a, b);
+		m.division(a, b);
+		m.sqr(a, b);
+		m.cube(a, b);
+		
+	}
+
+}
---
+++ Maths.java
@@ -0,0 +1,41 @@
+
+public class Maths {
+	public void add(int a, int b) {
+		int result=a +b;
+		System.out.println("Addition is :"+result);
+	}
+	
+	public void substract(int a, int b) {
+		int result=a -b;
+		System.out.println("Sustractiion is :"+result);
+	}
+	
+	public void multiplication(int a, int b) {
+		int result=a * b;
+		System.out.println("Multiplication is :"+result);
+	}
+	
+	
+	public void division(int a, int b) {
+		int result=a/b;
+		System.out.println("Division is :"+result);
+	}
+	
+	public void sqr(int a, int b) {
+		int result=a*a;
+		int result1=b*b;
+		System.out.println("Square of a is :"+result);
+		System.out.println("Square of b is :"+result1);
+	}
+	
+	public void cube(int a, int b) {
+		int result=a*a*a;
+		int result1=b*b*b;
+		System.out.println("Cube of a is :"+result);
+		System.out.println("Cube of b is :"+result1);
+
+
+	}
+
+
+}
